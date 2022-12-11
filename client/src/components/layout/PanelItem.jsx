8a7e70acf6dc266b3aaefa7c435ff96ea254fd72
Matheus Kunnen Ledesma <matheuskl@accionproactiva.com>
2022-12-11 14:04:31 -0300

    Panel item

---
+++ client/src/components/layout/PanelItem.jsx
@@ -0,0 +1,76 @@
+import { Box, Button, Typography, Menu, MenuItem } from '@mui/material';
+import React, { useState } from 'react';
+
+const PanelItem = ({ onRemove, index, title, children, ...props }) => {
+  const [width, setWidth] = useState(100);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (w) => {
+    if (!isNaN(w)) setWidth(w);
+    setAnchorEl(null);
+  };
+  return (
+    <Box
+      width={`${width}%`}
+      boxShadow={1}
+      borderRadius={1}
+      padding={'0.5rem'}
+      margin={'0.5rem'}
+      paddingTop={'0.25rem'}
+      {...props}
+    >
+      <Box display={'flex'} justifyContent="space-between">
+        <Typography variant="h5" sx={{ fontWeight: 'bold', flexGrow: 1 }}>
+          {String(title).toUpperCase()}
+        </Typography>
+        <Button id="config-btn" onClick={handleMenuClick}>
+          Config
+        </Button>
+        <Button onClick={() => onRemove()} color="error" sx={{ minWidth: 0 }}>
+          X
+        </Button>
+      </Box>
+      <Box>{children}</Box>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        MenuListProps={{
+          role: 'listbox',
+        }}
+      >
+        <MenuItem name="btn-25" onClick={() => handleClose(23)}>
+          25%
+        </MenuItem>
+        <MenuItem name="btn-50" onClick={() => handleClose(48)}>
+          50%
+        </MenuItem>
+        <MenuItem name="btn-75" onClick={() => handleClose(73)}>
+          75%
+        </MenuItem>
+        <MenuItem name="btn-100" onClick={() => handleClose(100)}>
+          100%
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+};
+
+PanelItem.defaultProps = {
+  onRemove: () => {},
+  title: '',
+};
+
+export default PanelItem;
